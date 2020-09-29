461a249f9868e112a38d493457f7e31236e865b4
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-09-29 00:40:12 -0500

    Created front-end folders

---
+++ library-frontend/src/index.js
@@ -0,0 +1,11 @@
+const BASE_URL = "http://localhost:3000"
+const BOOKS_URL = `${BASE_URL}/books`
+const LIBRARIES_URL = `${BASE_URL}/libraries`
+
+
+
+const main = () =>{
+    renderCatalog()
+}
+
+main()
