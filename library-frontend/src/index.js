e77fc4732d380656b520a20aa8bd728e82175eb3
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-09-29 13:24:42 -0500

    google maps implemented, basic html structure

--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -2,7 +2,25 @@ const BASE_URL = "http://localhost:3000"
 const BOOKS_URL = `${BASE_URL}/books`
 const LIBRARIES_URL = `${BASE_URL}/libraries`
 
+const renderCatalog = () => {
+    let mapToggle = true 
+    let button = document.querySelector('#toggle-map-catalog');
+    let libraryCatalog = document.querySelector('#library-catalog')
+    console.log(button)
+    let mapContainer = document.querySelector('#map-container')
+    button.addEventListener('click', () => {
+        mapToggle = !mapToggle
+        if (mapToggle){
+            mapContainer.style.display = "block";
+            libraryCatalog.style.display ='none'
 
+        }
+        else {
+            mapContainer.style.display = "none";
+            libraryCatalog.style.display ='block'
+        }
+    });
+}
 
 const main = () =>{
     renderCatalog()
