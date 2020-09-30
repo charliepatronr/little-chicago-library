28ac45b1d0d3975aed333f2b7ed8dbfadee7d947
Natalia <n8galan@gmail.com>
2020-09-30 14:05:48 -0500

    log ls layout bootstrap css

--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -2,18 +2,32 @@ const BASE_URL = "http://localhost:3000"
 const BOOKS_URL = `${BASE_URL}/books`
 const LIBRARIES_URL = `${BASE_URL}/libraries`
 
+const main = () =>{
+    renderCatalog()
+    addClickListener()
+}
+
+const addClickListener = () => {
+  let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+    keyboard: false
+  })
+  const libraryCatalog = document.querySelector('#library-catalog')
+  libraryCatalog.addEventListener('click', e => {
+  console.log(e);
+  myModal.toggle()
+  })
+}
+
 const renderCatalog = () => {
     let mapToggle = true 
     let button = document.querySelector('#toggle-map-catalog');
     let libraryCatalog = document.querySelector('#library-catalog')
-    console.log(button)
     let mapContainer = document.querySelector('#map-container')
     button.addEventListener('click', () => {
         mapToggle = !mapToggle
         if (mapToggle){
             mapContainer.style.display = "block";
             libraryCatalog.style.display ='none'
-
         }
         else {
             mapContainer.style.display = "none";
@@ -22,8 +36,5 @@ const renderCatalog = () => {
     });
 }
 
-const main = () =>{
-    renderCatalog()
-}
 
 main()
